b7aa8706d35928ed12e85c863831195e4f0851f9
Arun-SoftwareDeveloper <arunramasamy46@gmail.com>
2023-10-31 10:55:02 +0530

    new file:   .gitignore 	new file:   Controllers/UserController.js 	new file:   Models/UserForm.js 	new file:   README.md 	new file:   Routes/UserRoutes.js 	new file:   api.js 	new file:   package-lock.json 	new file:   package.json

---
+++ api.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const UserRoutes = require("./Routes/UserRoutes");
+const dbUrl =
+  "mongodb+srv://arunramasamy46:arunramasamy46@cluster0.etmdqsc.mongodb.net/?retryWrites=true&w=majority";
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+mongoose
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    // Start your server or perform any other operations here
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/", UserRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+const Port = 4000;
+app.listen(Port, () => {
+  console.log(`The server is running on the ${Port}`);
+});
